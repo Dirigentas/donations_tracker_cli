eafaebc93bd52e40a0ebbfb4cee63d331471493a
Aras <minelga.aras@gmail.com>
2024-03-02 10:51:39 +0200

    db update

--- src/CharityRepository.php
+++ src/CharityRepository.php
@@ -8,16 +8,37 @@ namespace Aras\DonationsTrackerCli;
 
 class CharityRepository {
     private $charities = [];
+    private $filePath;
 
-    public function addCharity(Charity $charity) {
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+        
+        if (file_exists($this->filePath)) {
+            $data = file_get_contents($this->filePath);
+            $charitiesData = json_decode($data, true);
+            if ($charitiesData) {
+                foreach ($charitiesData as $charityData) {
+                    $this->charities[] = new Charity($charityData['id'], $charityData['name'], $charityData['representativeEmail']);
+                }
+            }
+        }
+    }
+
+    public function addCharity(Charity $charity)
+    {
         $this->charities[] = $charity;
     }
 
-    public function getAllCharities() {
+    public function getAllCharities()
+    {
+        uasort($this->charities, fn ($a, $b) => $a['name'] <=> $b['name']);
+
         return $this->charities;
     }
 
-    public function getCharityById($id) {
+    public function getCharityById($id)
+    {
         foreach ($this->charities as $charity) {
             if ($charity->getId() === $id) {
                 return $charity;
@@ -26,7 +47,8 @@ class CharityRepository {
         return null;
     }
 
-    public function deleteCharity($id) {
+    public function deleteCharity($id)
+    {
         foreach ($this->charities as $key => $charity) {
             if ($charity->getId() === $id) {
                 unset($this->charities[$key]);
@@ -34,5 +56,18 @@ class CharityRepository {
             }
         }
         return false;
+    }
+
+    private function __destruct()
+    {
+        $charitiesData = [];
+        foreach ($this->charities as $charity) {
+            $charitiesData[] = [
+                'id' => $charity->getId(),
+                'name' => $charity->getName(),
+                'representativeEmail' => $charity->getRepresentativeEmail()
+            ];
+        }
+        file_put_contents($this->filePath, json_encode($charitiesData, JSON_PRETTY_PRINT));
     }
 }
